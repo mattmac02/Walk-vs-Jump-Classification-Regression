9e59a021e64a6eab677f2c26ecdb9d7038e6d5bb
mattmac02 <mattmac02@hotmail.ca>
2023-03-13 21:16:06 -0400

    More additions

--- main.py
+++ main.py
@@ -1,101 +1,167 @@
+
 import numpy as np
 import h5py
 import pandas as pd
 import os
 
-# define the directories and file names for the data
-base_dir = os.getcwd()
-walking_dir = os.path.join(base_dir, 'Walking_Data')
-jumping_dir = os.path.join(base_dir, 'Jumping_Data')
+# Matt's Data
+MrightHandW = pd.read_csv('Walking_Data/MrightHandW.csv')
+MleftBackPocketW = pd.read_csv('Walking_Data/MleftBackPocketW.csv')
+MleftJacketPocketW = pd.read_csv('Walking_Data/MleftJacketW.csv')
+MrightFrontPocketW = pd.read_csv('Walking_Data/MrightFrontPocketW.csv')
+MrightJacketPocketW = pd.read_csv('Walking_Data/MrightJacketW.csv')
 
-matt_files = {
-    'right_hand_walking': 'MrightHandW.csv',
-    'left_back_pocket_walking': 'MleftBackPocketW.csv',
-    'left_jacket_walking': 'MleftJacketW.csv',
-    'right_front_pocket_walking': 'MrightFrontPocketW.csv',
-    'right_jacket_walking': 'MrightJacketW.csv',
-    'right_hand_jumping': 'MrightHandJ.csv',
-    'left_back_pocket_jumping': 'MleftBackPocketJ.csv',
-    'left_jacket_jumping': 'MleftJacketPocketJ.csv',
-    'right_front_pocket_jumping': 'MrightFrontPocketJ.csv',
-    'right_jacket_jumping': 'MrightJacketPocketJ.csv',
-}
+MrightHandJ = pd.read_csv('Jumping_Data/MrightHandJ.csv')
+MleftBackPocketJ = pd.read_csv('Jumping_Data/MleftBackPocketJ.csv')
+MleftJacketPocketJ = pd.read_csv('Jumping_Data/MleftJacketPocketJ.csv')
+MrightFrontPocketJ = pd.read_csv('Jumping_Data/MrightFrontPocketJ.csv')
+MrightJacketPocketJ = pd.read_csv('Jumping_Data/MrightJacketPocketJ.csv')
 
-warren_files = {
-    'right_hand_walking': 'WrightHandW.csv',
-    'left_back_pocket_walking': 'WleftBackPocketW.csv',
-    'left_jacket_walking': 'WleftJacketPocketW.csv',
-    'right_front_pocket_walking': 'WrightFrontPocketW.csv',
-    'right_jacket_walking': 'WrightJacketPocketW.csv',
-    'right_hand_jumping': 'WrightHandJ.csv',
-    'left_back_pocket_jumping': 'WleftBackPocketJ.csv',
-    'left_jacket_jumping': 'WleftJacketPocketJ.csv',
-    'right_front_pocket_jumping': 'WrightFrontPocketJ.csv',
-    'right_jacket_jumping': 'WrightJacketPocketJ.csv',
-}
+# Warren's Data
+WrightHandW = pd.read_csv('Walking_Data/WrightHandW.csv')
+WleftBackPocketW = pd.read_csv('Walking_Data/WleftBackPocketW.csv')
+WleftJacketPocketW = pd.read_csv('Walking_Data/WleftJacketPocketW.csv')
+WrightFrontPocketW = pd.read_csv('Walking_Data/WrightFrontPocketW.csv')
+WrightJacketPocketW = pd.read_csv('Walking_Data/WrightJacketPocketW.csv')
 
-ellen_files = {
-    'right_hand_walking': 'ErightHandW.csv',
-    'left_back_pocket_walking': 'EleftBackPocketW.csv',
-    'left_jacket_walking': 'EleftJacketPocketW.csv',
-    'right_front_pocket_walking': 'ErightFrontPocketW.csv',
-    'right_jacket_walking': 'ErightJacketPocketW.csv',
-    'right_hand_jumping': 'ErightHandJ.csv',
-    'left_back_pocket_jumping': 'EleftBackPocketJ.csv',
-    'left_jacket_jumping': 'EleftJacketPocketJ.csv',
-    'right_front_pocket_jumping': 'ErightFrontPocketJ.csv',
-    'right_jacket_jumping': 'ErightJacketPocketJ.csv',
-}
+WrightHandJ = pd.read_csv('Jumping_Data/WrightHandJ.csv')
+WleftBackPocketJ = pd.read_csv('Jumping_Data/WleftBackPocketJ.csv')
+WleftJacketPocketJ = pd.read_csv('Jumping_Data/WleftJacketPocketJ.csv')
+WrightFrontPocketJ = pd.read_csv('Jumping_Data/WrightFrontPocketJ.csv')
+WrightJacketPocketJ = pd.read_csv('Jumping_Data/WrightJacketPocketJ.csv')
 
-# read in the data files for each subject and store them in a dictionary
-matt_data = {}
-for key, file in matt_files.items():
-    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
-    df = pd.read_csv(file_path)
-    matt_data[key] = df
+# Ellen's Data
+ErightHandW = pd.read_csv('Walking_Data/ErightHandW.csv')
+EleftBackPocketW = pd.read_csv('Walking_Data/EleftBackPocketW.csv')
+EleftJacketPocketW = pd.read_csv('Walking_Data/EleftJacketPocketW.csv')
+ErightFrontPocketW = pd.read_csv('Walking_Data/ErightFrontPocketW.csv')
+ErightJacketPocketW = pd.read_csv('Walking_Data/ErightJacketPocketW.csv')
 
-warren_data = {}
-for key, file in warren_files.items():
-    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
-    df = pd.read_csv(file_path)
-    warren_data[key] = df
+ErightHandJ = pd.read_csv('Jumping_Data/ErightHandJ.csv')
+EleftBackPocketJ = pd.read_csv('Jumping_Data/EleftBackPocketJ.csv')
+EleftJacketPocketJ = pd.read_csv('Jumping_Data/EleftJacketPocketJ.csv')
+ErightFrontPocketJ = pd.read_csv('Jumping_Data/ErightFrontPocketJ.csv')
+ErightJacketPocketJ = pd.read_csv('Jumping_Data/ErightJacketPocketJ.csv')
 
-ellen_data = {}
-for key, file in ellen_files.items():
-    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
-    df = pd.read_csv(file_path)
-    ellen_data[key] = df
+Matts_Dataset = pd.concat([MleftBackPocketW, MleftJacketPocketW, MrightFrontPocketW, MrightHandW, MrightJacketPocketW,
+                           MleftBackPocketJ, MleftJacketPocketJ, MrightFrontPocketJ, MrightHandJ, MrightJacketPocketJ])
 
-# combine the data into a single dataframe
-matt_df = pd.concat(matt_data.values(), axis=1, keys=matt_data.keys())
-warren_df = pd.concat(warren_data.values(), axis=1, keys=warren_data.keys())
-ellen_df = pd.concat(ellen_data.values(), axis=1, keys=ellen_data.keys())
-combined_df = pd.concat([matt_df, warren_df, ellen_df], axis=1, keys=['matt', 'warren', 'ellen'])
+Matts_Dataset.to_csv('Data/Matts_Data.csv')
 
+Warrens_Dataset = pd.concat([WleftBackPocketW, WleftJacketPocketW, WrightFrontPocketW, WrightHandW, WrightJacketPocketW,
+                             WleftBackPocketJ, WleftJacketPocketJ, WrightFrontPocketJ, WrightHandJ, WrightJacketPocketJ])
 
-# store the combined data in an HDF5 file
+Warrens_Dataset.to_csv('Data/Warrens_Data.csv')
+
+Ellens_Dataset = pd.concat([EleftBackPocketW, EleftJacketPocketW, ErightFrontPocketW, ErightHandW, ErightJacketPocketW,
+                            EleftBackPocketJ, EleftJacketPocketJ, ErightFrontPocketJ, ErightHandJ, ErightJacketPocketJ])
+
+Ellens_Dataset.to_csv('Data/Ellens_Data.csv')
+
+combined_data = pd.concat([
+    EleftBackPocketW,
+    EleftJacketPocketW,
+    ErightFrontPocketW,
+    ErightHandW,
+    ErightJacketPocketW,
+    MleftBackPocketW,
+    MleftJacketPocketW,
+    MrightFrontPocketW,
+    MrightHandW,
+    MrightJacketPocketW,
+    WleftBackPocketW,
+    WleftJacketPocketW,
+    WrightFrontPocketW,
+    WrightHandW,
+    WrightJacketPocketW,
+    EleftBackPocketJ,
+    EleftJacketPocketJ,
+    ErightFrontPocketJ,
+    ErightHandJ,
+    ErightJacketPocketJ,
+    MleftBackPocketJ,
+    MleftJacketPocketJ,
+    MrightFrontPocketJ,
+    MrightHandJ,
+    MrightJacketPocketJ,
+    WleftBackPocketJ,
+    WleftJacketPocketJ,
+    WrightFrontPocketJ,
+    WrightHandJ,
+    WrightJacketPocketJ,
+])
+
+combined_data.to_csv('combined.csv', index=False)
+
 with h5py.File('data.h5', 'w') as hdf:
-    # Matt's Data Group
+    # # Combined Dataset Creation
+    combined_DataSet = hdf.create_group('/mainDataset')
+    combined_DataSet.create_dataset('mainDataset', data=combined_data)
+
+    comb = pd.read_csv('combined.csv')
+    # comb['Time (s)'] = pd.to_datetime(comb['Time (s)'])
+    comb.set_index('Time (s)', inplace=True)
+    comb.to_csv('testfile.csv')
+
+    window_size = 500
+    segments = [comb.iloc[i:i+window_size] for i in range(0, len(comb), window_size)]
+    count_segments = int(np.ceil(len(comb) / window_size))
+
+    # Shuffle group elements
+    for i in range(count_segments):
+        segments[i] = segments[i].sample(frac=1).reset_index(drop=True)
+
+    # Matt's Dataset Creation
+    # Walking
     Matt_Group = hdf.create_group('/Matt_Group')
-    for key, value in matt_data.items():
-        # Create a new dataset in the group for each key-value pair
-        Matt_dataset = Matt_Group.create_dataset(key, data=value)
+    Matt_Group.create_dataset('mbrpw', data=MleftBackPocketW)
+    Matt_Group.create_dataset('mrhw', data=MrightHandW)
+    Matt_Group.create_dataset('mrjw', data=MrightJacketPocketW)
+    Matt_Group.create_dataset('mrfpw', data=MrightFrontPocketW)
+    Matt_Group.create_dataset('mljw', data=MleftJacketPocketW)
+    # Jumping
+    Matt_Group.create_dataset('mbrpj', data=MleftBackPocketJ)
+    Matt_Group.create_dataset('mrhj', data=MrightHandJ)
+    Matt_Group.create_dataset('mrjj', data=MrightJacketPocketJ)
+    Matt_Group.create_dataset('mrfpj', data=MrightFrontPocketJ)
+    Matt_Group.create_dataset('mljj', data=MleftJacketPocketJ)
 
-    # Warren's Data Group
+    # Warren's Dataset Creation
     Warren_Group = hdf.create_group('/Warren_Group')
-    for key, value in warren_data.items():
-        # Create a new dataset in the group for each key-value pair
-        Warren_dataset = Warren_Group.create_dataset(key, data=value)
+    Warren_Group.create_dataset('wbrpw', data=WleftBackPocketW)
+    Warren_Group.create_dataset('wrhw', data=WrightHandW)
+    Warren_Group.create_dataset('wrjw', data=WrightJacketPocketW)
+    Warren_Group.create_dataset('wrfpw', data=WrightFrontPocketW)
+    Warren_Group.create_dataset('wljw', data=WleftJacketPocketW)
+    # Jumping
+    Warren_Group.create_dataset('wbrpj', data=WleftBackPocketJ)
+    Warren_Group.create_dataset('wrhj', data=WrightHandJ)
+    Warren_Group.create_dataset('wrjj', data=WrightJacketPocketJ)
+    Warren_Group.create_dataset('wrfpj', data=WrightFrontPocketJ)
+    Warren_Group.create_dataset('wljj', data=WleftJacketPocketJ)
 
-    # Ellen's Data Group
+    # Ellen's Dataset Creation
     Ellen_Group = hdf.create_group('/Ellen_Group')
-    for key, value in ellen_data.items():
-        # Create a new dataset in the group for each key-value pair
-        Ellen_dataset = Ellen_Group.create_dataset(key, data=value)
+    Ellen_Group.create_dataset('ebrpw', data=EleftBackPocketW)
+    Ellen_Group.create_dataset('erhw', data=ErightHandW)
+    Ellen_Group.create_dataset('erjw', data=ErightJacketPocketW)
+    Ellen_Group.create_dataset('erfpw', data=ErightFrontPocketW)
+    Ellen_Group.create_dataset('eljw', data=EleftJacketPocketW)
+    # Jumping
+    Ellen_Group.create_dataset('ebrpj', data=EleftBackPocketJ)
+    Ellen_Group.create_dataset('erhj', data=ErightHandJ)
+    Ellen_Group.create_dataset('erjj', data=ErightJacketPocketJ)
+    Ellen_Group.create_dataset('erfpj', data=ErightFrontPocketJ)
+    Ellen_Group.create_dataset('eljj', data=EleftJacketPocketJ)
 
-    # Combining Datasets
-    group = hdf.create_group('combined_dataset')
-    # convert the pandas dataframe to a numpy array
-    combined_data = combined_df.to_numpy()
-    # write the numpy array to the HDF5 file
-    comb = group.create_dataset('combined_data', data=combined_data)
+    # # Testing HDF5 Output
+    # with h5py.File('data.h5', 'r') as hdf:
+    #     items = list(hdf.items())
+    #     print(items)
+    #     Matt_Group = hdf.get('/Warren_Group')
+    #     print(list(Matt_Group.items()))
+    #     d1 = Matt_Group.get('wljj')
+    #     d1 = np.array(d1)
+    #     print('\n')
+    #     print(d1.sEllen)
+
--- old.py
+++ old.py
@@ -1,105 +1,120 @@
-# # Matt's Data
-# MrightHandW = pd.read_csv('Walking_Data/MrightHandW.csv')
-# MbackRightPocketW = pd.read_csv('Walking_Data/MleftBackPocketW.csv')
-# MleftJacketW = pd.read_csv('Walking_Data/MleftJacketW.csv')
-# MrightFrontPocketW = pd.read_csv('Walking_Data/MrightFrontPocketW.csv')
-# MrightJacketW = pd.read_csv('Walking_Data/MrightJacketW.csv')
-#
-# MrightHandJ = pd.read_csv('Jumping_Data/MrightHandJ.csv')
-# MbackRightPocketJ = pd.read_csv('Jumping_Data/MleftBackPocketJ.csv')
-# MleftJacketJ = pd.read_csv('Jumping_Data/MleftJacketPocketJ.csv')
-# MrightFrontPocketJ = pd.read_csv('Jumping_Data/MrightFrontPocketJ.csv')
-# MrightJacketJ = pd.read_csv('Jumping_Data/MrightJacketPocketJ.csv')
-#
-# # Warren's Data
-# WrightHandW = pd.read_csv('Walking_Data/WrightHandW.csv')
-# WbackRightPocketW = pd.read_csv('Walking_Data/WleftBackPocketW.csv')
-# WleftJacketW = pd.read_csv('Walking_Data/WleftJacketPocketW.csv')
-# WrightFrontPocketW = pd.read_csv('Walking_Data/WrightFrontPocketW.csv')
-# WrightJacketW = pd.read_csv('Walking_Data/WrightJacketPocketW.csv')
-#
-# WrightHandJ = pd.read_csv('Jumping_Data/WrightHandJ.csv')
-# WbackRightPocketJ = pd.read_csv('Jumping_Data/WleftBackPocketJ.csv')
-# WleftJacketJ = pd.read_csv('Jumping_Data/WleftJacketPocketJ.csv')
-# WrightFrontPocketJ = pd.read_csv('Jumping_Data/WrightFrontPocketJ.csv')
-# WrightJacketJ = pd.read_csv('Jumping_Data/WrightJacketPocketJ.csv')
-#
-# # Ellen's Data
-# ErightHandW = pd.read_csv('Walking_Data/ErightHandW.csv')
-# EbackRightPocketW = pd.read_csv('Walking_Data/EleftBackPocketW.csv')
-# EleftJacketW = pd.read_csv('Walking_Data/EleftJacketW.csv')
-# ErightFrontPocketW = pd.read_csv('Walking_Data/ErightFrontPocketW.csv')
-# ErightJacketW = pd.read_csv('Walking_Data/ErightJacketW.csv')
-#
-# ErightHandJ = pd.read_csv('Jumping_Data/ErightHandJ.csv')
-# EbackRightPocketJ = pd.read_csv('Jumping_Data/EbackRightPocketJ.csv')
-# EleftJacketJ = pd.read_csv('Jumping_Data/EleftJacketPocketJ.csv')
-# ErightFrontPocketJ = pd.read_csv('Jumping_Data/ErightFrontPocketJ.csv')
-# ErightJacketJ = pd.read_csv('Jumping_Data/ErightJacketPocketJ.csv')
-#
-#
-# with h5py.File('data.h5', 'w') as hdf:
-#     # # Combined Dataset Creation
-#     # combined_DataSet = hdf.create_group('/mainDataset')
-#     # combined_DataSet.create_dataset('mainDataset', data=matrix_2)
-#     #
-#     # trainSet = hdf.create_group('/mainDataset/train')
-#     # trainSet.create_dataset('mainDataset', data=matrix_2)
-#     #
-#     # testSet = hdf.create_group('/mainDataset/test')
-#     # testSet.create_dataset('mainDataset', data=matrix_2)
-#
-#     # Matt's Dataset Creation
-#     # Walking
-#     Matt_Group = hdf.create_group('/Matt_Group')
-#     Matt_Group.create_dataset('mbrpw', data=MbackRightPocketW)
-#     Matt_Group.create_dataset('mrhw', data=MrightHandW)
-#     Matt_Group.create_dataset('mrjw', data=MrightJacketW)
-#     Matt_Group.create_dataset('mrfpw', data=MrightFrontPocketW)
-#     Matt_Group.create_dataset('mljw', data=MleftJacketW)
-#     # Jumping
-#     Matt_Group.create_dataset('mbrpj', data=MbackRightPocketJ)
-#     Matt_Group.create_dataset('mrhj', data=MrightHandJ)
-#     Matt_Group.create_dataset('mrjj', data=MrightJacketJ)
-#     Matt_Group.create_dataset('mrfpj', data=MrightFrontPocketJ)
-#     Matt_Group.create_dataset('mljj', data=MleftJacketJ)
-#
-#     # Warren's Dataset Creation
-#     Warren_Group = hdf.create_group('/Warren_Group')
-#     Warren_Group.create_dataset('wbrpw', data=WbackRightPocketW)
-#     Warren_Group.create_dataset('wrhw', data=WrightHandW)
-#     Warren_Group.create_dataset('wrjw', data=WrightJacketW)
-#     Warren_Group.create_dataset('wrfpw', data=WrightFrontPocketW)
-#     Warren_Group.create_dataset('wljw', data=WleftJacketW)
-#     # Jumping
-#     Warren_Group.create_dataset('wbrpj', data=WbackRightPocketJ)
-#     Warren_Group.create_dataset('wrhj', data=WrightHandJ)
-#     Warren_Group.create_dataset('wrjj', data=WrightJacketJ)
-#     Warren_Group.create_dataset('wrfpj', data=WrightFrontPocketJ)
-#     Warren_Group.create_dataset('wljj', data=WleftJacketJ)
-#
-#     # Ellen's Dataset Creation
-#     Ellen_Group = hdf.create_group('/Ellen_Group')
-#     Ellen_Group.create_dataset('ebrpw', data=EbackRightPocketW)
-#     Ellen_Group.create_dataset('erhw', data=ErightHandW)
-#     Ellen_Group.create_dataset('erjw', data=ErightJacketW)
-#     Ellen_Group.create_dataset('erfpw', data=ErightFrontPocketW)
-#     Ellen_Group.create_dataset('eljw', data=EleftJacketW)
-#     # Jumping
-#     Ellen_Group.create_dataset('ebrpj', data=EbackRightPocketJ)
-#     Ellen_Group.create_dataset('erhj', data=ErightHandJ)
-#     Ellen_Group.create_dataset('erjj', data=ErightJacketJ)
-#     Ellen_Group.create_dataset('erfpj', data=ErightFrontPocketJ)
-#     Ellen_Group.create_dataset('eljj', data=EleftJacketJ)
-#
-#     # Testing HDF5 Output
-#     with h5py.File('data.h5', 'r') as hdf:
-#         items = list(hdf.items())
-#         print(items)
-#         Matt_Group = hdf.get('/Warren_Group')
-#         print(list(Matt_Group.items()))
-#         d1 = Matt_Group.get('wljj')
-#         d1 = np.array(d1)
-#         print('\n')
-#         print(d1.sEllen)
+import numpy as np
+import h5py
+import pandas as pd
+import os
 
+# define the directories and file names for the data
+base_dir = os.getcwd()
+walking_dir = os.path.join(base_dir, 'Walking_Data')
+jumping_dir = os.path.join(base_dir, 'Jumping_Data')
+
+matt_files = {
+    'right_hand_walking': 'MrightHandW.csv',
+    'left_back_pocket_walking': 'MleftBackPocketW.csv',
+    'left_jacket_walking': 'MleftJacketW.csv',
+    'right_front_pocket_walking': 'MrightFrontPocketW.csv',
+    'right_jacket_walking': 'MrightJacketW.csv',
+    'right_hand_jumping': 'MrightHandJ.csv',
+    'left_back_pocket_jumping': 'MleftBackPocketJ.csv',
+    'left_jacket_jumping': 'MleftJacketPocketJ.csv',
+    'right_front_pocket_jumping': 'MrightFrontPocketJ.csv',
+    'right_jacket_jumping': 'MrightJacketPocketJ.csv',
+}
+
+warren_files = {
+    'right_hand_walking': 'WrightHandW.csv',
+    'left_back_pocket_walking': 'WleftBackPocketW.csv',
+    'left_jacket_walking': 'WleftJacketPocketW.csv',
+    'right_front_pocket_walking': 'WrightFrontPocketW.csv',
+    'right_jacket_walking': 'WrightJacketPocketW.csv',
+    'right_hand_jumping': 'WrightHandJ.csv',
+    'left_back_pocket_jumping': 'WleftBackPocketJ.csv',
+    'left_jacket_jumping': 'WleftJacketPocketJ.csv',
+    'right_front_pocket_jumping': 'WrightFrontPocketJ.csv',
+    'right_jacket_jumping': 'WrightJacketPocketJ.csv',
+}
+
+ellen_files = {
+    'right_hand_walking': 'ErightHandW.csv',
+    'left_back_pocket_walking': 'EleftBackPocketW.csv',
+    'left_jacket_walking': 'EleftJacketPocketW.csv',
+    'right_front_pocket_walking': 'ErightFrontPocketW.csv',
+    'right_jacket_walking': 'ErightJacketPocketW.csv',
+    'right_hand_jumping': 'ErightHandJ.csv',
+    'left_back_pocket_jumping': 'EleftBackPocketJ.csv',
+    'left_jacket_jumping': 'EleftJacketPocketJ.csv',
+    'right_front_pocket_jumping': 'ErightFrontPocketJ.csv',
+    'right_jacket_jumping': 'ErightJacketPocketJ.csv',
+}
+
+all_files ={
+    'right_hand_walking': 'MrightHandW.csv',
+    'left_back_pocket_walking': 'MleftBackPocketW.csv',
+    'left_jacket_walking': 'MleftJacketW.csv',
+    'right_front_pocket_walking': 'MrightFrontPocketW.csv',
+    'right_jacket_walking': 'MrightJacketW.csv',
+    'right_hand_jumping': 'MrightHandJ.csv',
+    'left_back_pocket_jumping': 'MleftBackPocketJ.csv',
+    'left_jacket_jumping': 'MleftJacketPocketJ.csv',
+    'right_front_pocket_jumping': 'MrightFrontPocketJ.csv',
+    'right_jacket_jumping': 'MrightJacketPocketJ.csv',
+
+}
+
+# read in the data files for each subject and store them in a dictionary
+matt_data = {}
+for key, file in matt_files.items():
+    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
+    df = pd.read_csv(file_path)
+    matt_data[key] = df
+
+warren_data = {}
+for key, file in warren_files.items():
+    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
+    df = pd.read_csv(file_path)
+    warren_data[key] = df
+
+ellen_data = {}
+for key, file in ellen_files.items():
+    file_path = os.path.join(walking_dir if 'walking' in key else jumping_dir, file)
+    df = pd.read_csv(file_path)
+    ellen_data[key] = df
+
+# combine the data into a single dataframe
+matt_df = pd.concat(matt_data.values())
+warren_df = pd.concat(warren_data.values())
+ellen_df = pd.concat(ellen_data.values())
+combined_df = pd.concat([matt_df, warren_df, ellen_df])
+# print(combined_df)
+
+combined_df.to_csv('test2.csv')
+
+
+
+
+# store the combined data in an HDF5 file
+with h5py.File('data.h5', 'w') as hdf:
+    # Matt's Data Group
+    Matt_Group = hdf.create_group('/Matt_Group')
+    Matt_Group.create_dataset('matts', data=matt_df)
+
+    # Warren's Data Group
+    Warren_Group = hdf.create_group('/Warren_Group')
+    tee = Warren_Group.create_dataset('Warren_data', data=warren_df)
+
+    # Ellen's Data Group
+    Ellen_Group = hdf.create_group('/Ellen_Group')
+    Ellen_Group.create_dataset('Ellen_data', data=ellen_df)
+
+    # Combining Datasets
+    group = hdf.create_group('combined_dataset')
+    # convert the pandas dataframe to a numpy array
+    combined_data = combined_df.to_numpy()
+    # write the numpy array to the HDF5 file
+    comb = group.create_dataset('combined_data', data=combined_data)
+
+    combined_df['Time (s)'] = pd.to_datetime((combined_df['Time (s)']))
+    # print(combined_df['Time (s)'])
+
+
+    # print(comb)
